3e2c4f52d4a4064b8cc3d37f70dc02b5074ce357
Rohen <mailme.rohen@gmail.com>
2016-08-28 18:22:19 +0400

    Interest Rate Calculation

---
+++ InterestRate.py
@@ -0,0 +1,19 @@
+from numpy import *
+from matplotlib.pyplot import *
+
+x0 = 100 #intial amount
+p = 5 #interest rate
+N = 4 #number of years
+
+index_set = range(N+1)
+x = zeros(len(index_set))
+
+#Computing the solution
+
+x[0] = x0
+for n in index_set[1:]:
+  x[n] = x[n-1] + (p/100.0)*x[n-1]
+print x
+
+plot(index_set, x)
+plot.show()
